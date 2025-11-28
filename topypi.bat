90b12d5ce84be599c7361dcc08bfed534338c3f5
mistertayodimon <dimondimonych1@outlook.com>
2025-11-28 14:35:11 +0200

    feat: updated sending to pypi script

--- topypi.bat
+++ topypi.bat
@@ -1,5 +1,12 @@
 @echo off
-REM Batchfile which send this library on PyPI
+REM Script which send this library on PyPI
+
+REM Deleting sending cache directories
+rmdir dist /s /q
+rmdir msound.egg-info /s /q
+
+REM Sending to PyPI
 python -m build
 twine upload dist/*
+
 exit
